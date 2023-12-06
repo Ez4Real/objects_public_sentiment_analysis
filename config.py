f2a8515018a9aa42f811f080710cc99101c2b51d
Ez4Real <butilka05roma@gmail.com>
2023-12-06 22:03:51 +0200

    init RoBERTa model && hotel sentiment analyse embedding

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent
+
+dotenv_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path)
+
+
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_ENV = os.getenv('PINECONE_ENV')
